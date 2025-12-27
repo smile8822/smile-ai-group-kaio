d013226341055edeb5d2e3889906cbce1202b706
smile8822 <mgcms0502@gmail.com>
2025-12-28 04:43:50 +0900

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,11 @@
-import type { Config } from "tailwindcss";
-const config: Config = {
-  content: ["./app/**/*.{ts,tsx}"],
-  theme: { extend: {} },
-  plugins: []
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 };
-export default config;
